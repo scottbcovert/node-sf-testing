5ed60356edf0a6bc90f7461d8f04e7d53d422477
Scott Covert <scott.b.covert@gmail.com>
2014-10-15 18:40:59 -0400

    Added jsforce

--- index.js
+++ index.js
@@ -5,7 +5,17 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+
+	var jsforce = require('jsforce');
+	var conn = new jsforce.Connection();
+	conn.login('scott.covert@df14.com', 'dfpassw0rdDAAtkhl4gx2pqFXueP10ZhivL', function(err, res) {
+	  if (err) { return console.error(err); }
+	  conn.query('SELECT Id, Name FROM Account ORDER BY Name DESC', function(err, res) {
+	    if (err) { return console.error(err); }
+	    console.log(res);
+	  });
+	});
+	  
 })
 
 app.listen(app.get('port'), function() {
